f0846fa3019c11ced123f654c91d3c5a4e1cbbca
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-10-15 23:52:34 +0600

    feat: increase waiting time weight to 4.0 and reduce other reward coefficients

--- drl/config.py
+++ drl/config.py
@@ -49,7 +49,7 @@ class DRLConfig:
     # ALPHA_PED_DEMAND = 1.0
 
     # In drl/config.py - REBALANCED FOR VEHICLE WAITING TIME FOCUS
-    ALPHA_WAIT = 3.0       # INCREASED from 2.0 - Make waiting time even MORE DOMINANT
+    ALPHA_WAIT = 4.0       # INCREASED from 2.0 - Make waiting time even MORE DOMINANT
     ALPHA_SYNC = 0.3       # REDUCED from 0.85 - Too easy to game
     ALPHA_EMISSION = 0.03  # Reduced - less important than waiting
     ALPHA_EQUITY = 0.03    # Reduced - less important than waiting  
